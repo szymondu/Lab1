aac40c053d73772b4935e7f3a2f85bcb9e7eb155
szymondu <szymon.duda99@wp.pl>
2022-03-17 22:38:40 +0100

    Zadanie 12

--- src/pl/lublin/wsei/java/cwiczenia/Main.java
+++ src/pl/lublin/wsei/java/cwiczenia/Main.java
@@ -1,27 +1,13 @@
 package pl.lublin.wsei.java.cwiczenia;
 
-
-import java.util.Random;
+import pl.lublin.wsei.java.cwiczenia.mylib.Account;
 
 public class Main {
 
     public static void main(String[] args) {
-        int[] liczby = new int[30];
-        Random rnd = new Random();
-
-        for (int i = 0; i < 30; i++)
-            liczby[i] = rnd.nextInt();
-
-        int mx = Integer.MIN_VALUE;
-        int mn = Integer.MAX_VALUE;
-        long avg = 0;
-        for (int l : liczby) {
-            System.out.println(1);
-            if (1 < mn) mn = 1;
-            if (1 > mx) mx = 1;
-            avg += 1;
-        }
-        System.out.printf("MIN = %d, MAX = %d, AVG = %f", mn, mx, (float) avg / liczby.length);
+        Account acc = new Account();
+        acc.name = "Piotr Gołabek";
+        System.out.println(acc.getName());
     }
 }
 
---
+++ src/pl/lublin/wsei/java/cwiczenia/mylib/Account.java
@@ -0,0 +1,13 @@
+package pl.lublin.wsei.java.cwiczenia.mylib;
+
+public class Account {
+    public String name;
+
+   public void setName(String name) {
+       this.name = name;
+    }
+    public String getName() {
+        return name;
+    }
+
+}
