b4cc3a19bf643b3883583bf3032c509a2d05dd77
szymondu <szymon.duda99@wp.pl>
2022-03-17 20:32:38 +0100

    Zadanie 4

--- src/pl/lublin/wsei/java/cwiczenia/Main.java
+++ src/pl/lublin/wsei/java/cwiczenia/Main.java
@@ -3,6 +3,6 @@ package pl.lublin.wsei.java.cwiczenia;
 public class Main {
 
     public static void main(String[] args) {
-	System.out.println("Hello Java World!\n");
+    System.out.println("Hello Java world\n");
     }
 }
