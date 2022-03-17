b24cf501b632142f3a1841298f7815b9f29f166b
szymondu <szymon.duda99@wp.pl>
2022-03-17 20:39:35 +0100

    Zadanie 7

--- src/pl/lublin/wsei/java/cwiczenia/Main.java
+++ src/pl/lublin/wsei/java/cwiczenia/Main.java
@@ -3,6 +3,9 @@ package pl.lublin.wsei.java.cwiczenia;
 public class Main {
 
     public static void main(String[] args) {
-    System.out.println("Hello Java world\n");
+    int a = 3;
+    double b = 4.21;
+    String s = "Jakiś tekst";
+    System.out.printf("a = %d, b = %f, s=%20s %n", a, b, s);
     }
 }
