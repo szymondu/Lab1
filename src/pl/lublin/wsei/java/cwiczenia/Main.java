2d8f0c371d844c999d3442e01684ac38be4791ab
szymondu <szymon.duda99@wp.pl>
2022-03-17 20:48:00 +0100

    Zadanie 7g2

--- src/pl/lublin/wsei/java/cwiczenia/Main.java
+++ src/pl/lublin/wsei/java/cwiczenia/Main.java
@@ -3,9 +3,9 @@ package pl.lublin.wsei.java.cwiczenia;
 public class Main {
 
     public static void main(String[] args) {
-    int a = 3;
-    double b = 4.21;
-    String s = "Jakiś tekst";
-    System.out.printf("a = %d, b = %f, s=%20s %n", a, b, s);
+    System.out.printf("alfa \tsin(alfa\n)");
+    for (int i = 0; i < 370; i += 10){
+        System.out.printf("%d\t%f\t\n", i, Math.sin(i / 360.0 * 2 * Math.PI));
+    }
     }
 }
