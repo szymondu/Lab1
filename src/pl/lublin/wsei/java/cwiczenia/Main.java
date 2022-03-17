3a13898fabc91a9c5f9081cafdf1c56be48792d0
szymondu <szymon.duda99@wp.pl>
2022-03-17 21:01:34 +0100

    Zadanie 9d

--- src/pl/lublin/wsei/java/cwiczenia/Main.java
+++ src/pl/lublin/wsei/java/cwiczenia/Main.java
@@ -6,11 +6,15 @@ public class Main {
 
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
-        int num1, num2;
-        System.out.print("Podaj pierwsza liczbę: ");
-        num1 = input.nextInt();
-        System.out.print("Podaj drugą liczbę: ");
-        num2 = input.nextInt();
-        System.out.printf("Wynik dodawania %d + %d = %d%n", num1, num2, num1 + num2);
+        int num1 = 0, num2 = 0;
+        do {
+            System.out.print("Podaj pierwsza liczbę: ");
+            num1 = input.nextInt();
+            if (num1 ==0) break;
+            System.out.print("Podaj drugą liczbę: ");
+            num2 = input.nextInt();
+            if (num1 ==0) break;
+            System.out.printf("Wynik dodawania %d + %d = %d%n", num1, num2, num1 + num2);
+        } while (true);
     }
 }
