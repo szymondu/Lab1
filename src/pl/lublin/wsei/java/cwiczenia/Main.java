4f3f391f6e34abfad533f48a75062398361adc66
szymondu <szymon.duda99@wp.pl>
2022-03-17 20:53:38 +0100

    Zadanie 8

--- src/pl/lublin/wsei/java/cwiczenia/Main.java
+++ src/pl/lublin/wsei/java/cwiczenia/Main.java
@@ -1,11 +1,16 @@
 package pl.lublin.wsei.java.cwiczenia;
 
+import java.util.Scanner;
+
 public class Main {
 
     public static void main(String[] args) {
-    System.out.printf("alfa \tsin(alfa\n)");
-    for (int i = 0; i < 370; i += 10){
-        System.out.printf("%d\t%f\t\n", i, Math.sin(i / 360.0 * 2 * Math.PI));
-    }
+        Scanner input = new Scanner(System.in);
+        int num1, num2;
+        System.out.print("Podaj pierwsza liczbę: ");
+        num1 = input.nextInt();
+        System.out.print("Podaj drugą liczbę: ");
+        num2 = input.nextInt();
+        System.out.printf("Wynik dodawania %d + %d = %d%n", num1, num2, num1 + num2);
     }
 }
