a4b977b9a3581945d7325d7f466af224db00001e
szymondu <szymon.duda99@wp.pl>
2022-03-17 21:08:19 +0100

    Zadanie 9e

--- src/pl/lublin/wsei/java/cwiczenia/Main.java
+++ src/pl/lublin/wsei/java/cwiczenia/Main.java
@@ -8,12 +8,10 @@ public class Main {
         Scanner input = new Scanner(System.in);
         int num1 = 0, num2 = 0;
         do {
-            System.out.print("Podaj pierwsza liczbę: ");
+            System.out.print("Podaj liczbę, które mam dodać: ");
             num1 = input.nextInt();
-            if (num1 ==0) break;
-            System.out.print("Podaj drugą liczbę: ");
             num2 = input.nextInt();
-            if (num1 ==0) break;
+            if ((num1 == 0) || (num2 == 0) ) break;
             System.out.printf("Wynik dodawania %d + %d = %d%n", num1, num2, num1 + num2);
         } while (true);
     }
